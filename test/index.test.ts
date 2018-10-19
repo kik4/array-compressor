fd0ad76c93a8657019f4cafa66e5f2c993fa4238
kik4 <7163720+kik4@users.noreply.github.com>
2018-10-19 21:44:18 +0900

    add test with objects

--- test/index.test.ts
+++ test/index.test.ts
@@ -11,10 +11,17 @@ describe("compress", () => {
     ]
     expect(compress(array)).toEqual(expected)
   })
+
   it("compress(null) to throw error", () => {
     expect(() => {
       compress(null)
     }).toThrow()
+  })
+
+  it("compress(object) to equal apart objects", () => {
+    const array = [{ name: "A" }, { name: "A" }]
+    const expected: compressedItem[] = [{ value: { name: "A" }, count: 1 }, { value: { name: "A" }, count: 1 }]
+    expect(compress(array)).toEqual(expected)
   })
 })
 
@@ -29,9 +36,20 @@ describe("decompress", () => {
     const array = [1, 1, 1, 2, 2, 3, 4, 4, 4, 4]
     expect(decompress(expected)).toEqual(array)
   })
+
   it("decompress(null) to throw error", () => {
     expect(() => {
       decompress(null)
     }).toThrow()
+  })
+
+  it("decompress(array) to equal same objects", () => {
+    const expected: compressedItem[] = [{ value: { name: "B" }, count: 2 }]
+    const array = [{ name: "B" }, { name: "B" }]
+    const result = decompress(expected)
+    expect(result).toEqual(array)
+
+    result[0].name = "C"
+    expect(result[0] === result[1]).toBeTruthy()
   })
 })
